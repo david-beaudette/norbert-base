06b93e60f50194be43d2f83b676ce15a224d3fde
david-beaudette <david.beaudette@usherbrooke.ca>
2016-03-01 19:50:32 -0500

    Added header and sequence of operations.

---
+++ controle_par_mannette/controle_par_mannette.ino
@@ -0,0 +1,26 @@
+/////////////////////////////////////////////////////////////
+//
+// norbert-base -> controle_par_manette
+//
+// Permet de contrôler la base Tonka de Norbert en utilisant
+// une mannette fabriquée en Lego
+//
+// David Beaudette (2016)
+//
+/////////////////////////////////////////////////////////////
+
+void setup() {
+  // Initialisation des sorties vers les moteurs
+  
+  // Initialisation des entrées de la mannette
+  
+  // Initialisation de l'entrée analogique
+
+  // Initialisation du PID de la direction
+  
+}
+
+void loop() {
+  // 
+
+}
